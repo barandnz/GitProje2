cb5bac0161c1477ef0682714c575b15798311a39
barandeniz <baran-dnz@hotmail.comgit config --global user.email>
2022-07-14 22:08:12 +0300

    Ogrenci Classi ikinci bolum yapildi

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,8 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dunya");
+
+
+        System.out.println("MErhaba dunya 2");
+        System.out.println("Merhaba dunya 3");
     }
 }
